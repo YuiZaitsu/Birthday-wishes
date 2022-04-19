6843aa088a01390677789bef021979cbd5ef5816
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2022-04-19 15:47:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-var canvas = new fabric.Canvas('myCanvas');
- var x= document.getElementById("myAudio");
+Webcam.set({
+width:350,
+height:300,
+image_format:"png",
+png_quality:90
+});
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
 
-function new_image()
-{
-	
-	
+function take_snapshot() {
+Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML = "<img src = '"+ data_uri +"' id='captured_Image'>";
+});
 }
 
-function playSound(){
-	
+console.log("ml5version", ml5.version);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json",modelLoaded);
+
+
+function modelLoaded(){
+console.log("modelLoaded");
 }

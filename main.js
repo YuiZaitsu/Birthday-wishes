8d0a5a025d490ea608768b19f343afdfa33ad4c6
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2022-12-23 13:22:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-Webcam.set({
-width:350,
-height:300,
-image_format:"png",
-png_quality:90
-});
-camera = document.getElementById("camera");
-Webcam.attach("#camera");
-
-function take_snapshot() {
-Webcam.snap(function(data_uri){
-document.getElementById("result").innerHTML = "<img src = '"+ data_uri +"' id='captured_Image'>";
-});
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
 }
 
-console.log("ml5version", ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json",modelLoaded);
 
 
-function modelLoaded(){
-console.log("modelLoaded");
-}
